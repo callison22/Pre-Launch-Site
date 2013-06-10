febb9a1a18438d33a6fd913f1887deaabf8b8505
claire allison <callison22@gmail.com>
2013-06-10 15:16:09 -0400

    implement 'request invitation' feature

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,5 +9,34 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   # attr_accessible :role_ids, :as => :admin
   attr_accessible :name, :email, :password, :password_confirmation, :remember_me
+
+  # override Devise method
+  # no password is required when the account is created; validate password when the user sets one
+  validates_confirmation_of :password
+  def password_required?
+    if !persisted?
+      !(password != "")
+    else
+      !password.nil? || !password_confirmation.nil?
+    end
+  end
+  
+  # override Devise method
+  def confirmation_required?
+    false
+  end
+
+  # override Devise method
+  def active_for_authentication?
+    confirmed? || confirmation_period_valid?
+  end
+
+  def send_reset_password_instructions
+    if self.confirmed?
+      super
+    else
+      errors.add :base, "You must receive an invitation before you set your password."
+    end
+  end
   
 end
